5cb0a9e826be63a41a228a3aebaca0017433d832
youyi <youyichannel@163.com>
2024-10-04 20:34:41 +0800

    feat(init): init project

---
+++ src/test/java/com/youyi/gateway/BaseUnitTest.java
@@ -0,0 +1,16 @@
+package com.youyi.gateway;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author yoyocraft
+ * @date 2024/10/04
+ */
+@RunWith(MockitoJUnitRunner.class)
+public abstract class BaseUnitTest {
+
+    protected static final Logger LOGGER = LoggerFactory.getLogger(BaseUnitTest.class);
+}
